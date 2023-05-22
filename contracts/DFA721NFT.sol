c509f5220b9a4a3f0d2fd3fd1cc8deb1f3a95248
kingyoto <kingyoto@king.com>
2023-05-22 16:48:13 +0800

    update base uri

--- contracts/DFA721NFT.sol
+++ contracts/DFA721NFT.sol
@@ -23,9 +23,30 @@ contract DFA721NFT is Governable, ERC721 {
     string memory symbol, 
     string memory baseURI
   ) ERC721(name, symbol) public {
-    _setBaseURI(baseURI);
+    string memory addressStr = _toAsciiString(address(this));
+    _setBaseURI(string(abi.encodePacked(baseURI, "0x", addressStr, "/")));
     artist[msg.sender] = true;
     super.initialize(msg.sender);
+  }
+
+  function _toAsciiString(address x) private pure returns (string memory) {
+      bytes memory s = new bytes(40);
+      for (uint256 i = 0; i < 20; i++) {
+          bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2**(8*(19 - i)))));
+          bytes1 hi = bytes1(uint8(b) / 16);
+          bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+          s[2*i] = _char(hi);
+          s[2*i + 1] = _char(lo);
+      }
+      return string(s);
+  }
+
+  function _char(bytes1 b) private pure returns (bytes1 c) {
+      if (uint8(b) < 10) {
+          return bytes1(uint8(b) + 0x30);
+      } else {
+          return bytes1(uint8(b) + 0x57);
+      }
   }
 
   function setBaseURI(string memory baseURI) external governance returns (bool) {
